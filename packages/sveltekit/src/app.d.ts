b76d9bf5afe03da2c71d48dfc90acc4bbc276a8b
jay-dee7 <me@jsdp.dev>
2023-03-02 19:50:25 +0530

    feat: sveltekit sdk for logto

---
+++ packages/sveltekit/src/app.d.ts
@@ -0,0 +1,20 @@
+import type { UserInfoResponse } from '@logto/client';
+import BaseClient from '@logto/client';
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Platform {}
+		interface Locals {
+			getUser(): Promise<UserInfoResponse | null>;
+			user: UserInfoResponse | null;
+			logto: BaseClient;
+		}
+		interface PageData {
+			user: UserInfoResponse | null;
+		}
+	}
+}
+
+export {};
---
+++ packages/sveltekit/src/lib/index.ts
@@ -0,0 +1,57 @@
+import BaseClient, { createRequester } from '@logto/client';
+import { CookieStore } from './storage.js';
+import { generateCodeChallenge, generateCodeVerifier, generateState } from './generators.js';
+import { redirect } from '@sveltejs/kit';
+import type { LogtoConfig } from '@logto/client';
+import type { Handle } from '@sveltejs/kit';
+
+export type { LogtoConfig, LogtoClientErrorCode, UserInfoResponse } from '@logto/client';
+
+export {
+    LogtoError,
+    OidcError,
+    Prompt,
+    LogtoRequestError,
+    LogtoClientError,
+    ReservedScope,
+    UserScope,
+} from '@logto/client';
+
+const navigate = (url: string) => {
+    throw redirect(307, url);
+};
+
+export type LogtoSvelteConfig = LogtoConfig & {
+    fetch: typeof fetch;
+};
+
+export class LogtoClient extends BaseClient {
+    constructor(config: LogtoSvelteConfig, cookieStore: CookieStore) {
+        const requester = createRequester(config.fetch);
+        super(config, {
+            requester,
+            navigate,
+            storage: cookieStore,
+            generateCodeChallenge,
+            generateCodeVerifier,
+            generateState,
+        });
+    }
+}
+
+export const LogtoAuthHandler = (appId: string, endpoint: string, scopes?: string[]): Handle => {
+    return async function ({ event, resolve }) {
+        const client = new LogtoClient(
+            {
+                appId: appId,
+                endpoint: endpoint,
+                fetch: event.fetch,
+                scopes: scopes,
+            },
+            new CookieStore(appId, event.cookies)
+        );
+        event.locals.logto = client;
+
+        return resolve(event);
+    };
+};
