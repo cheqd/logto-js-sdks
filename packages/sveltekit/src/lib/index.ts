f3db0f3e54d2cc564a25afb8ab0a875bf466696b
jay-dee7 <me@jsdp.dev>
2023-06-20 15:01:20 +0530

    feat: allow multiple resources in Hook Handler

--- packages/sveltekit/src/lib/index.ts
+++ packages/sveltekit/src/lib/index.ts
@@ -1,57 +1,68 @@
 import BaseClient, { createRequester } from '@logto/client';
-import { CookieStore } from './storage.js';
-import { generateCodeChallenge, generateCodeVerifier, generateState } from './generators.js';
-import { redirect } from '@sveltejs/kit';
 import type { LogtoConfig } from '@logto/client';
+import { redirect } from '@sveltejs/kit';
 import type { Handle } from '@sveltejs/kit';
+
+import { generateCodeChallenge, generateCodeVerifier, generateState } from './generators.js';
+import { CookieStore } from './storage.js';
 
 export type { LogtoConfig, LogtoClientErrorCode, UserInfoResponse } from '@logto/client';
 
+export { CookieStore } from './storage.js';
+
 export {
-    LogtoError,
-    OidcError,
-    Prompt,
-    LogtoRequestError,
-    LogtoClientError,
-    ReservedScope,
-    UserScope,
+  LogtoError,
+  OidcError,
+  Prompt,
+  LogtoRequestError,
+  LogtoClientError,
+  ReservedScope,
+  UserScope,
 } from '@logto/client';
 
 const navigate = (url: string) => {
-    throw redirect(307, url);
+  // eslint-disable-next-line @typescript-eslint/no-throw-literal
+  throw redirect(307, url);
 };
 
 export type LogtoSvelteConfig = LogtoConfig & {
-    fetch: typeof fetch;
+  fetch: typeof fetch;
 };
 
 export class LogtoClient extends BaseClient {
-    constructor(config: LogtoSvelteConfig, cookieStore: CookieStore) {
-        const requester = createRequester(config.fetch);
-        super(config, {
-            requester,
-            navigate,
-            storage: cookieStore,
-            generateCodeChallenge,
-            generateCodeVerifier,
-            generateState,
-        });
-    }
+  constructor(config: LogtoSvelteConfig, cookieStore: CookieStore) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const requester = createRequester(config.fetch);
+    super(config, {
+      requester,
+      navigate,
+      storage: cookieStore,
+      generateCodeChallenge,
+      generateCodeVerifier,
+      generateState,
+    });
+  }
 }
 
-export const LogtoAuthHandler = (appId: string, endpoint: string, scopes?: string[]): Handle => {
-    return async function ({ event, resolve }) {
-        const client = new LogtoClient(
-            {
-                appId: appId,
-                endpoint: endpoint,
-                fetch: event.fetch,
-                scopes: scopes,
-            },
-            new CookieStore(appId, event.cookies)
-        );
-        event.locals.logto = client;
+export const LogtoAuthHandler = (
+  appId: string,
+  endpoint: string,
+  scopes?: string[],
+  resources?: string[]
+): Handle => {
+  return async function ({ event, resolve }) {
+    const client = new LogtoClient(
+      {
+        appId,
+        endpoint,
+        fetch: event.fetch,
+        scopes,
+        resources,
+      },
+      new CookieStore(appId, event.cookies)
+    );
+    event.locals.logto = client;
 
-        return resolve(event);
-    };
+    return resolve(event);
+  };
 };
--- packages/sveltekit/src/lib/storage.ts
+++ packages/sveltekit/src/lib/storage.ts
@@ -6,58 +6,54 @@ import type { CookieSerializeOptions } from 'cookie';
 export const logtoStorageItemKeyPrefix = `logto`;
 
 const defaultCookieOptions: CookieSerializeOptions = {
-    path: '/',
-    httpOnly: true,
-    sameSite: 'lax',
-}
+  path: '/',
+  httpOnly: true,
+  sameSite: 'lax',
+};
 
 export class CookieStore implements Storage {
-    private readonly storageKey: string;
-    private cookies: Cookies;
-    private readonly cookieOptions: CookieSerializeOptions;
+  private readonly storageKey: string;
+  private readonly cookies: Cookies;
+  private readonly cookieOptions: CookieSerializeOptions;
 
-    constructor(appId: string, cookies: Cookies, cookieOptions?: CookieSerializeOptions) {
-        this.storageKey = `${logtoStorageItemKeyPrefix}:${appId}`;
-        this.cookies = cookies;
-        if (cookieOptions) {
-            this.cookieOptions = cookieOptions
-        } else {
-            this.cookieOptions = defaultCookieOptions
-        }
+  constructor(appId: string, cookies: Cookies, cookieOptions?: CookieSerializeOptions) {
+    this.storageKey = `${logtoStorageItemKeyPrefix}:${appId}`;
+    this.cookies = cookies;
+    this.cookieOptions = cookieOptions ?? defaultCookieOptions;
+  }
+
+  async getItem(key: StorageKey): Promise<Nullable<string>> {
+    if (key === 'signInSession') {
+      const cookie = this.cookies.get(this.storageKey);
+      if (cookie) {
+        return cookie;
+      }
+
+      return null;
     }
 
-    async getItem(key: StorageKey): Promise<Nullable<string>> {
-        if (key === 'signInSession') {
-            const cookie = this.cookies.get(this.storageKey);
-            if (cookie) {
-                return cookie;
-            }
-
-            return null;
-        }
-
-        const cookie = this.cookies.get(`${this.storageKey}:${key}`);
-        if (cookie) {
-            return decodeURIComponent(cookie);
-        }
-
-        return null;
+    const cookie = this.cookies.get(`${this.storageKey}:${key}`);
+    if (cookie) {
+      return decodeURIComponent(cookie);
     }
 
-    async setItem(key: StorageKey, value: string): Promise<void> {
-        if (key === 'signInSession') {
-            this.cookies.set(this.storageKey, value, this.cookieOptions);
-            return;
-        }
-        this.cookies.set(`${this.storageKey}:${key}`, value, this.cookieOptions);
-    }
+    return null;
+  }
 
-    async removeItem(key: StorageKey): Promise<void> {
-        if (key === 'signInSession') {
-            this.cookies.delete(this.storageKey);
-
-            return;
-        }
-        return this.cookies.delete(`${this.storageKey}:${key}`);
+  async setItem(key: StorageKey, value: string): Promise<void> {
+    if (key === 'signInSession') {
+      this.cookies.set(this.storageKey, value, this.cookieOptions);
+      return;
     }
+    this.cookies.set(`${this.storageKey}:${key}`, value, this.cookieOptions);
+  }
+
+  async removeItem(key: StorageKey): Promise<void> {
+    if (key === 'signInSession') {
+      this.cookies.delete(this.storageKey);
+
+      return;
+    }
+    this.cookies.delete(`${this.storageKey}:${key}`);
+  }
 }
